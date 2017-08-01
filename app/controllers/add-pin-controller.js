b7dbe4c1efc4b010a3a40747c743ec74ac088d53
Sam <samb283@gmail.com>
2017-08-01 13:51:12 -0500

    controller for adding pins - version 1 - time 1350 8/1/2017

--- app/controllers/add-pin-controller.js
+++ app/controllers/add-pin-controller.js
@@ -2,6 +2,20 @@
 
 pinHead.controller('AddPinController', function ($scope, $window, UserFactory, PinFactory) {
 
+    $scope.boards = PinFactory.getBoards();
 
+    $scope.pin = {
+        imgUrl: "",
+        board_id: "",
+        title: ""
+    };
+
+    $scope.addNewPin = ()=>{
+        PinFactory.postNewPin($scope.pin)
+        .then((data)=>{
+            console.log("new pin data", data);
+            $window.location.href = `#!/board/${$scope.pin.board_id}`;
+        });
+    };
 
 });
