deb92f2b704a2ac473e384847f58fab62adfab67
Arwa <arwa.it15@gmail.com>
2017-08-01 11:01:35 -0500

    sign in with google

--- app/controllers/user-controller.js
+++ app/controllers/user-controller.js
@@ -1,2 +1,10 @@
 'use strict';
 
+pinHead.controller('UserController', function($scope, $window, UserFactory) {
+    $scope.login = () => {
+        UserFactory.loginUser()
+        .then( (data) => {
+            $window.location.href = '#!/board/all';
+        });
+    };
+});
---
+++ app/factories/user-factory.js
@@ -0,0 +1,39 @@
+'use strict';
+
+pinHead.factory('UserFactory', function($q, $http, FirebaseUrl, FBCreds) {
+
+    var provider = new firebase.auth.GoogleAuthProvider();
+    // https://pinheads-1f7c1.firebaseapp.com/__/auth/handler
+    var config = {
+        apiKey: FBCreds.apiKey,
+        authDomain: FBCreds.authDomain
+    };
+
+    firebase.initializeApp(config);
+
+    let currentUser = null;
+
+    let isAuthenticated = () => {
+        return $q( (resolve, reject) => {
+            firebase.auth().onAuthStateChanged( (user) => {
+                if(user) {
+                    currentUser = user.uid;
+                    console.log("currentUser", currentUser );
+                    // return currentUser;
+                    resolve(true);
+                }
+                else { //on logout we need to set it back to null.
+                    currentUser = null;
+                    resolve(false);
+                }
+            });
+        });
+    };
+
+    let loginUser = () => {
+        console.log("auth?");
+        return firebase.auth().signInWithPopup( provider);
+    };
+
+return {loginUser};
+});
