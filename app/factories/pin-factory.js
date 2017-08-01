a297cdcee1d32ac62ccea407fd4be44616e28a09
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-08-01 11:33:41 -0500

    add new pin function added

--- app/factories/pin-factory.js
+++ app/factories/pin-factory.js
@@ -3,10 +3,12 @@
 pinHead.factory("PinFactory", function($q, $http, FirebaseUrl) {
 	
 	//this needs to be called after the user is authenticated - on login automatically, and if called again pass in curren user's uid
-	let getBoards = (uid) => {
+	let getBoards = (user) => {
 		return $q( (resolve, reject) => {
-			$http.get(`${FirebaseUrl}boards.json?orderBy="uid"&equalTo="${uid}"`)
+			//may need to put "" around the "user" variable because it needs to be a string, not a number. 
+			$http.get(`${FirebaseUrl}boards.json?orderBy="uid"&equalTo=${user}`)
 			.then( (boardsData) => {
+				console.log("board data", boardsData.data);
 				resolve(boardsData.data);
 			})
 			.catch( (err) => {
@@ -15,11 +17,13 @@ pinHead.factory("PinFactory", function($q, $http, FirebaseUrl) {
 		});
 	};
 
-	let getPins = (uid) => {
+	let getPins = (user) => {
 		return $q( (resolve, reject) => {
-			//we can only fetch based on one parameter (like UID), but we only really need the pins for one board at a time. Fetch all, then filter within the controller?
-			$http.get(`${FirebaseUrl}pins.json?orderBy="uid"&equalTo="${uid}"`)
+			console.log("user?", user);
+			//we can only fetch based on one parameter (like UID), but we only really need the pins for one board at a time. Fetch all, then filter within the controller
+			$http.get(`${FirebaseUrl}pins.json?orderBy="uid"&equalTo=${user}`)
 			.then( (pinsData) => {
+				console.log("pins data", pinsData.data);
 				resolve(pinsData.data);
 			})
 			.catch( (err) => {
@@ -28,6 +32,23 @@ pinHead.factory("PinFactory", function($q, $http, FirebaseUrl) {
 		});
 	};
 
+	// needs to take in the info from the form and controller for NEW item
+	let postNewPin = (newObj) => {
+		return $q( (resolve, reject) => {
+			$http.post(`${FirebaseUrl}pins.json`,
+				angular.toJson(newObj))
+			.then( (response) => {
+				resolve(response);
+			})
+			.catch( (err) => {
+				reject(err);
+			});
+		});
+	};
+
+//test:
+getPins('4444');
+getBoards('4444');
 
 	return { getBoards, getPins };
 });
--- app/factories/user-factory.js
+++ app/factories/user-factory.js
@@ -2,6 +2,11 @@
 
 pinHead.factory("UserFactory", function($q, $http, FirebaseUrl) {
 
+  let consoleStuff = function() {
+    console.log('sturr');
+  };
 
-	
+  
+  return {consoleStuff};
+
 });
