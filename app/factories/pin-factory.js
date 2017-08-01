012034655692b7fa16dd40cae094fc6d66c00c17
Arwa <arwa.it15@gmail.com>
2017-08-01 11:40:19 -0500

    merge conflict

--- app/factories/pin-factory.js
+++ app/factories/pin-factory.js
@@ -1,1 +1,23 @@
 'use strict';
+
+
+pinHead.factory("PinFactory", function($q, $http, FirebaseUrl) {
+	
+	//this needs to be called after the user is authenticated - on login automatically, and if called again pass in curren user's uid
+	let getBoards = (uid) => {
+		return $q( (resolve, reject) => {
+			$http.get(`${FirebaseUrl}boards.json?orderBy="uid"&equalTo="${uid}"`)
+			.then( (boardsData) => {
+				resolve(boardsData);
+			})
+			.catch( (err) => {
+				reject(err);
+			});
+		});
+	};
+
+	// let getPins = ()
+
+
+	return { getBoards };
+});
