0548529428a3baf4a217a73b415492cfd41dfc28
Megan B <brown.m.j@outlook.com>
2017-08-01 14:57:37 -0500

    user sees own boards on login

--- app/controllers/all-boards-controller.js
+++ app/controllers/all-boards-controller.js
@@ -2,6 +2,32 @@
 
 pinHead.controller('AllBoardsController', function ($scope, $window, UserFactory, PinFactory) {
 
+	let currentUser = null;
 
+	UserFactory.isAuthenticated()
+	.then( (user) => {
+		currentUser = UserFactory.getUser();
+		console.log("current user???", currentUser);
+		goGetBoards();
+	});
+
+
+function goGetBoards() {
+	PinFactory.getBoards(currentUser)
+	.then( (boards) => {
+		console.log("board data data", boards);
+		let boardsArr = [];
+		let boardData = boards;
+		Object.keys(boardData).forEach( (key) => {
+			boardData[key].id = key;
+			boardsArr.push(boardData[key]);
+		});
+		console.log("boards Array", boardsArr);
+		$scope.boards = boardsArr;
+	})
+	.catch( (err) => {
+		console.log("error", err);
+	});
+}
 
 });
