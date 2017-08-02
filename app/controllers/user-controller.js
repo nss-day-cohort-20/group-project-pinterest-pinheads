97f0160bd94001870d8e4d85fc50d17e2496736d
Megan B <brown.m.j@outlook.com>
2017-08-02 10:16:51 -0500

    show/hide divs when user adds board

--- app/controllers/user-controller.js
+++ app/controllers/user-controller.js
@@ -5,6 +5,7 @@ pinHead.controller('UserController', function($scope, $window, UserFactory) {
         UserFactory.loginUser()
         .then( (data) => {
             let currentUser = data.user.uid;
+            console.log("user data", data);
             console.log("currentUser", currentUser);
             $window.location.href = '#!/board/all';
         });
