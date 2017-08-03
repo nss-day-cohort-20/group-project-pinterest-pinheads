941a114eedec2215232324c2965032e28ff819a3
Sam <samb283@gmail.com>
2017-08-03 10:11:23 -0500

    promise all working by some miracle

--- app/controllers/all-boards-controller.js
+++ app/controllers/all-boards-controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-pinHead.controller('AllBoardsController', function ($scope, $window, UserFactory, PinFactory) {
+pinHead.controller('AllBoardsController', function ($scope, $window, UserFactory, PinFactory, $q) {
 
 	let currentUser = null;
 
@@ -21,12 +21,23 @@ function goGetBoards() {
 	.then( (boards) => {
 		let boardsArr = [];
 		let boardData = boards;
+		let promisesArr = [];
 		Object.keys(boardData).forEach( (key) => {
 			boardData[key].id = key;
 			boardsArr.push(boardData[key]);
 		});
 		console.log("boards Array", boardsArr);
 		$scope.boards = boardsArr;
+		boardsArr.forEach((board)=>{
+			let promiseCall = PinFactory.getPins(board.id);
+			// console.log("promiseCall", promiseCall);
+			promisesArr.push(promiseCall);
+			// console.log("promisesArr", promisesArr);
+		});
+		$q.all(promisesArr)
+		.then((values)=>{
+			console.log("values", values);
+		});
 	})
 	.catch( (err) => {
 		console.log("error", err);
