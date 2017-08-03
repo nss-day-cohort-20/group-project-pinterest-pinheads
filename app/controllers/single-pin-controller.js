68b73863243e15c3f9f11a1fadf867113231cf3f
thejonroberts <jon.roberts.8@gmail.com>
2017-08-03 13:15:37 -0500

    added margin to pin/board view div

---
+++ app/controllers/single-pin-controller.js
@@ -0,0 +1,30 @@
+'use strict';
+
+pinHead.controller('SinglePinController', function ($scope, $window, $routeParams, UserFactory, PinFactory) {
+
+	let pinId = $routeParams.pin_id;
+	let boardId=null;
+
+	PinFactory.getSinglePin(pinId)
+	.then( (onePin) => {
+		$scope.singlePin = onePin;
+		boardId = onePin.board_id;
+	});
+
+	$scope.delete = () => {
+		if ($window.confirm("Are you sure you want to delete?")) {
+			PinFactory.deletePinFromFB(pinId)
+			.then( (response) => {
+				//go back to single board view
+				$window.location.href=`#!/board/${boardId}`;
+				$window.location.reload(true);
+
+			});
+		}
+	};
+
+	$scope.editPin = () => {
+		$window.location.href=`#!/pin/edit/${pinId}`;
+	};
+
+});
